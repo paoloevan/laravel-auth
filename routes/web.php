9dc07f80a6c1ac9262ddc4d8a161407304ae7f19
paoloevan <paoloev1900@gmail.com>
2023-01-10 16:30:00 +0100

    add ProjectController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,6 +26,7 @@ Route::middleware('auth', 'verified')
     ->prefix('admin')
     ->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('projects', ProjectController::class);
     });
 
 require __DIR__ . '/auth.php';
