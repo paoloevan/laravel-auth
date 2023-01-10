7183d913145194124d55c383581552b061fb9289
paoloevan <paoloev1900@gmail.com>
2023-01-10 17:19:30 +0100

    add nuttons and views

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -14,20 +14,28 @@
                 <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Team</th>
+                        <th scope="col">Duration</th>
+                        <th scope="col">Description</th>
                     </tr>
                 </thead>
                 <tbody>
+
+                    @foreach ($projects as $project)
                     <tr>
-                        <td>1,001</td>
-                        <td>random</td>
-                        <td>data</td>
-                        <td>placeholder</td>
-                        <td>text</td>
+                        <td>{{$project->id}}</td>
+                        <td>{{$project->title}}</td>
+                        <td>d{{$project->team}}ata</td>
+                        <td>{{$project->duration}}</td>
+                        <td>{{$project->description}}</td>
+                        <td>
+                            <a class="p-1 my-1 btn btn-primary" href="{{route('admin.projects.show', $project->id)}}">Show</a>
+                            <a class="p-1 my-1 btn btn-secondary" href="{{route('admin.projects.edit', $project->id)}}">Edit</a>
+                            <a class="p-1 my-1 btn btn-danger" href="{{route('admin.projects.destroy', $project->id)}}">Delete</a>
+                        </td>
                     </tr>
+                    @endforeach
 
                 </tbody>
             </table>
